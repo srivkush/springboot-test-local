22687bb449ca7aeb90f75a3cd873a9d394d50135
Kushagra <kushagra11.srivastava@dell.com>
2018-08-06 16:31:15 +0530

    Updated

--- src/test/java/com/nttdata/test/AbstractBoosterApplicationTest.java
+++ src/test/java/com/nttdata/test/AbstractBoosterApplicationTest.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package io.openshift.booster;
+package com.nttdata.test;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.core.Is.is;
